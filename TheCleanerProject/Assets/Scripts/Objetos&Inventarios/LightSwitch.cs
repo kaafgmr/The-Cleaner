5479db9c93263cd898760e5597b73b06c6285237
Kelson Alan Alberti Filho <kelsonfilho7@hotmail.com>
2023-04-13 13:24:02 +0200

    light switch rotation started

--- TheCleanerProject/Assets/Scripts/Objetos&Inventarios/LightSwitch.cs
+++ TheCleanerProject/Assets/Scripts/Objetos&Inventarios/LightSwitch.cs
@@ -5,8 +5,26 @@ using UnityEngine;
 public class LightSwitch : MonoBehaviour
 {
     [SerializeField] Light lightToSwitch;
+
+    Quaternion offRotation;
+    Quaternion onRotation;
+
+    private void Start()
+    {
+        onRotation = transform.parent.rotation;
+        offRotation = Quaternion.Euler(-onRotation.eulerAngles.x, onRotation.eulerAngles.y, 180);
+    }
     private void OnTriggerEnter(Collider other)
     {
         lightToSwitch.enabled = !lightToSwitch.enabled;
+
+        if (lightToSwitch.enabled)
+        {
+            transform.parent.rotation = onRotation;
+        }
+        else
+        {
+            transform.parent.rotation = offRotation;
+        }
     }
 }
