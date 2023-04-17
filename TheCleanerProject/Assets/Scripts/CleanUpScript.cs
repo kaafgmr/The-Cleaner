98ee903f14b962d7aae5e0892afde693ae99fc1a
Kelson Alan Alberti Filho <kelsonfilho7@hotmail.com>
2023-04-17 09:53:48 +0200

    map separation done

--- TheCleanerProject/Assets/Scripts/CleanUpScript.cs
+++ TheCleanerProject/Assets/Scripts/CleanUpScript.cs
@@ -1,20 +1,14 @@
 #if UNITY_EDITOR
-
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 [ExecuteAlways]
 public class CleanUpScript : MonoBehaviour
 {
-    //public GameObject parentToClean;
-    public GameObject[] objects;
-    public Material[] materials;
+    public GameObject parentToClean;
+    public string nameToSelect;
     public bool Activate = false;
-
-    Dictionary<string, Material> materialDictionary = new Dictionary<string, Material>();
 
     private void Update()
     {
@@ -29,36 +23,28 @@ public class CleanUpScript : MonoBehaviour
         if (Activate == false) return;
         Activate = false;
 
-        setUpDictionary();
+        Transform[] objects = parentToClean.GetComponentsInChildren<Transform>(true);
+        List<GameObject> all = new List<GameObject>();
 
         for (int i = 0; i < objects.Length; i++)
         {
-            if (!objects[i].TryGetComponent(out MeshRenderer MR)) continue;
-            
-            Material[] ObjMaterials = MR.sharedMaterials;
-
-            if (ObjMaterials.Length > 0)
+            if (objects[i].name.Contains(nameToSelect))
             {
-                Debug.Log(ObjMaterials.Length);
-                for (int j = 0; j < ObjMaterials.Length; j++)
-                {
-                    if (!materialDictionary.ContainsKey(ObjMaterials[j].name)) continue;
-                    ObjMaterials[j] = materialDictionary[ObjMaterials[j].name];
-                }
-
-                MR.sharedMaterials = ObjMaterials;
+                all.Add(objects[i].gameObject);
             }
         }
-    }
 
-    void setUpDictionary()
-    {
-        for (int i = 0; i < materials.Length; i++)
+        GameObject[] select = all.ToArray();
+
+        Selection.activeGameObject = gameObject;
+
+        if (select.Length > 0)
         {
-            if (!materialDictionary.ContainsKey(materials[i].name))
-            {
-                materialDictionary.Add(materials[i].name, materials[i]); 
-            }
+            Selection.objects = select;
+        }
+        else
+        {
+            Debug.Log(select.Length + " objects selected");
         }
     }
 }
