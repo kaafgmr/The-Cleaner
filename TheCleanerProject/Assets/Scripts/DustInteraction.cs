b8ca4fc6f328d36ec0ef13e1d814434406388f95
Victor Donet Sogorb <v.donet.sogorb@gmail.com>
2023-04-18 11:38:22 +0200

    Dust & Vacuum Task Updated

--- TheCleanerProject/Assets/Scripts/DustInteraction.cs
+++ TheCleanerProject/Assets/Scripts/DustInteraction.cs
@@ -7,12 +7,42 @@ public class DustInteraction : MonoBehaviour
     public bool hasBeenVacuumed;
     void Start()
     {
-        
+        hasBeenVacuumed = false;   
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.TryGetComponent(out VacuumTask vacuumTask))
+        {
+            if (!vacuumTask.CheckIfItsDone())
+            {
+                StartCoroutine(isVacuuming());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out VacuumTask vacuumTask))
+        {
+            if (!vacuumTask.CheckIfItsDone())
+            {
+                StopCoroutine(isVacuuming());
+            }
+        }
+    }
+
+    IEnumerator isVacuuming()
+    {
+        yield return new WaitForSeconds(2);
+        hasBeenVacuumed = true;
+    }
+
+    private void Update()
+    {
+        if (hasBeenVacuumed)
+        {
+            Destroy(gameObject);
+        }
     }
 }
