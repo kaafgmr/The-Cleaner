bcf4ebdb408cc75e5c16c309b8ddaaa1a670b2a1
Kelson Alan Alberti Filho <kelsonfilho7@hotmail.com>
2023-05-10 14:01:43 +0200

    close window task done and some dynamic colliders fixed

--- TheCleanerProject/Assets/Scripts/Tasks/CloseWindowInteraction.cs
+++ TheCleanerProject/Assets/Scripts/Tasks/CloseWindowInteraction.cs
@@ -1,35 +1,9 @@
-using Oculus.Interaction;
-using Oculus.Interaction.HandGrab;
-using Oculus.Interaction.Input;
 using UnityEngine;
 
 public class CloseWindowInteraction : MonoBehaviour
 {
-    OneGrabTranslateTransformer ogtt;
-    Hand hand;
-    Vector3 openPos;
-
-    private void Start()
+    public void FinishTask()
     {
-        ogtt = GetComponent<OneGrabTranslateTransformer>();
-        openPos = transform.position;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        collision.gameObject.TryGetComponent(out hand);
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        hand = null;
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (hand != null && transform.position.y == openPos.y - ogtt.Constraints.MaxY.Value)
-        {
-            GetComponent<HandGrabInteractable>().enabled = false;
-        }
+        CloseWindowsTask.instance.UpdateTask();
     }
 }
